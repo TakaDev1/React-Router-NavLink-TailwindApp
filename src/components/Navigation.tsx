a1d9d40416ff7cba3a123b5d36ab4a8e43e867c4
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-18 17:21:28 +0900

    style: Project Adjustment Design

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,10 +2,25 @@ import React from "react";
 import { NavLink } from "react-router";
 const Navigation = () => {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/profile">Profile</NavLink>
-      <NavLink to="/settings">Settings</NavLink>
+    <nav className="my-10 w-1/2 flex bg-white mx-auto justify-around py-2 rounded-xl">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? "text-blue-500 font-bold" : "text-gray-500")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/profile"
+        className={({ isActive }) => (isActive ? "text-blue-500 font-bold" : "text-gray-500")}
+      >
+        Profile
+      </NavLink>
+      <NavLink
+        to="/settings"
+        className={({ isActive }) => (isActive ? "text-blue-500 font-bold" : "text-gray-500")}
+      >
+        Settings
+      </NavLink>
     </nav>
   );
 };
